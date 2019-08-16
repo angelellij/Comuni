74a3055482602cbb36eb801c71e620aaec9c8806
javier <angelellij@gmail.com>
2019-08-16 16:13:31 -0300

    coneccion a firebase hecha
    	modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {AngularFireModule} from '@angular/fire';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
+import {AngularFirestoreModule, FirestoreSettingsToken} from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-            AngularFireModule.initializeApp(environment.firebase)],
+            AngularFireModule.initializeApp(environment.firebase),
+            AngularFirestoreModule],
   providers: [
     StatusBar,
     SplashScreen,
